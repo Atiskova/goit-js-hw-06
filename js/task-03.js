0938f24e7dce7721c3c46f1978949f9eff0cce93
Anna Atiskova <anna.atiskova@gmail.com>
2022-12-23 00:42:17 +0200

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,27 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryListEl = document.querySelector('.gallery');
+galleryListEl.setAttribute("style", "list-style-type:none; display: flex; flex-wrap: wrap; gap: 30px;")
+
+// galleryListEl.style.list-style = "none";
+console.log(galleryListEl)
+// Функція для створення карточки makeGalleryCard(cardInfo)
+const makeGalleryCard = ({  url, alt } = {}) => {
+  return `
+    <li class="gallery-item">
+      <a href="#">
+        <img src="${url}" alt="${alt}" width = auto height = 500>
+      </a>
+    </li>
+  `;
+};
+
+// Створення масиву рядків із елементами
+const galleryCards = images.map((el, idx, arr) => {
+  return makeGalleryCard(el);
+});
+
+// Вставка елементів на сторінку
+galleryListEl.insertAdjacentHTML('beforeend', galleryCards.join(''));
