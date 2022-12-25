3ff6730fc2d3949a94c25aa75a0094ba86693d71
Anna Atiskova <anna.atiskova@gmail.com>
2022-12-25 23:37:22 +0200

    task4

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const decrementBtn = document.querySelector("[data-action='decrement']");
+const incrementBtn = document.querySelector("[data-action='increment']");
+const value = document.querySelector("#value")
+  
+  let counterValue = 0;
+  const increment = () => {
+    counterValue += 1;
+  
+    value.textContent = counterValue;
+  };
+  
+  const decrement = () => {
+    counterValue -= 1;
+  
+    value.textContent = counterValue;
+  };
+  
+  
+  decrementBtn.addEventListener("click", decrement);
+  incrementBtn.addEventListener("click", increment);
+
