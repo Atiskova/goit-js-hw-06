8a93a627dabe0a3f4c791012b31d8737428042fc
Anna Atiskova <anna.atiskova@gmail.com>
2022-12-26 00:07:38 +0200

    task5

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+const userNameInputEl = document.querySelector('#name-input');
+const userNameOutputEl = document.querySelector('#name-output');
+
+userNameInputEl.addEventListener('blur', event => {
+  console.log('Blur Event');
+});
+
+userNameInputEl.addEventListener('input', event => {
+  console.log('Input Event');
+
+  if (userNameInputEl.value === '') {
+    userNameOutputEl.textContent = 'Anonymous';
+
+    return;
+  }
+
+  userNameOutputEl.textContent = userNameInputEl.value;
+});
+
