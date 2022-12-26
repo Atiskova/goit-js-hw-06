fab1f2b50eb64f5ea68706bc053dabeb8a575db6
Anna Atiskova <anna.atiskova@gmail.com>
2022-12-27 00:10:13 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const contactFormEl = document.querySelector('.login-form');
 
+contactFormEl.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const {
+        elements: { email, password },
+    } = event.currentTarget
+
+    if (email.value === '' || password.value === '') {
+        return alert `Всі поля повинні бути заповнені`
+    }
+
+    console.log({ email: email.value, Password: password.value });
+  
+    contactFormEl.reset();
+  
+  });
